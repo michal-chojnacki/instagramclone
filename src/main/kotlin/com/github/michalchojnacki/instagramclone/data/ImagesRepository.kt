2cadd6aea02fcb3d91b42408d4200f5ebaa5a535
Michal Chojnacki <michal.chojnacki@intive.com>
2020-05-01 18:19:23 +0200

    Updates ImagesRepository

--- src/main/kotlin/com/github/michalchojnacki/instagramclone/data/ImagesRepository.kt
+++ src/main/kotlin/com/github/michalchojnacki/instagramclone/data/ImagesRepository.kt
@@ -7,31 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
 @Repository
 class ImagesRepository @Autowired constructor(private val imageUrlMapper: ImageUrlMapper) {
 
-    fun saveContentImage(image: MultipartFile, id: Long) : Result<Image> {
+    fun saveContentImage(image: MultipartFile, id: Long): Result<Image> {
         val resource = ImagesRepository::class.java.getResource("/static/images/contents")
-        val newFile = File("${resource.path}/$id.jpg")
-
-        try {
-            if (!newFile.exists()) {
-                newFile.createNewFile()
-            }
-            var read = 0
-            val bytes = ByteArray(1024)
-            val outputStream = FileOutputStream(newFile)
-            val inputStream = image.inputStream
-            while (inputStream.read(bytes).also { read = it } != -1) {
-                outputStream.write(bytes, 0, read)
-            }
-        } catch (e: IOException) {
-            return Result.Error(e)
-        }
-
+        image.transferTo(File("${resource.path}/$id.jpg"))
         return Result.Success(imageUrlMapper.map(id))
     }
 }
